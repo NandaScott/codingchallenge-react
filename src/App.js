5bcb6dd271ad717d3ab9c2c1948bfa3e7cbf8609
Nanda Scott <nanda1123@gmail.com>
2018-07-24 15:59:58 -0400

    Can now render a list based on a button click.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,82 @@ for (let i = 0; i < rn({min:1, max: 15, integer:true}); i++) {
   payload.push({name: randomNames(), values: arr});
 }
 
+function ShowOptions () {
+  return (
+    <ol>
+      <li>Create Factory</li>
+      <li>Rename Factory</li>
+      <li>Generate Numbers</li>
+      <li>Delete Factory</li>
+    </ol>
+  );
+}
+
+function HideOptions () {
+  return (null);
+}
+
+function HandleOptions (props) {
+  const renderOptions = props.renderOptions;
+
+  if (renderOptions) {
+    return <ShowOptions />;
+  }
+
+  return <HideOptions />;
+}
+
+function ShowOptionsButton (props) {
+  return (
+    <button onClick={props.onClick}>
+      Show Options
+    </button>
+  );
+}
+
+function HideOptionsButton (props) {
+  return (
+    <button onClick={props.onClick}>
+      Hide Options
+    </button>
+  );
+}
+
+class OptionsControl extends Component {
+  constructor (props) {
+    super(props);
+    this.handleShowOptionsClick = this.handleShowOptionsClick.bind(this);
+    this.handleHideOptionsClick = this.handleHideOptionsClick.bind(this);
+    this.state = {isShown: false};
+  }
+
+  handleShowOptionsClick () {
+    this.setState({isShown: true});
+  }
+
+  handleHideOptionsClick () {
+    this.setState({isShown: false});
+  }
+
+  render () {
+    const isShown = this.state.isShown;
+    let button;
+
+    if (isShown) {
+      button = <HideOptionsButton onClick={this.handleHideOptionsClick} />;
+    } else {
+      button = <ShowOptionsButton onClick={this.handleShowOptionsClick} />;
+    }
+
+    return (
+      <div className='options-button'>
+        <HandleOptions renderOptions={isShown} />
+        {button}
+      </div>
+    );
+  }
+}
+
 function MapPayload (props) {
   return props.payload.map((anObjectMapped, index) => {
     let listValues = anObjectMapped.values.map((values, index) => 
@@ -23,7 +99,11 @@ function MapPayload (props) {
 
     return(
       <ol key={index}>
-        <li key={index}><span className="list-header"><h4>{anObjectMapped.name}</h4></span>
+        <li key={index}>
+          <div className="list-header">
+            <h4>{anObjectMapped.name}</h4>
+            <OptionsControl />
+          </div>
           <ol key={index}>
             {listValues}
           </ol>
@@ -36,7 +116,11 @@ function MapPayload (props) {
 function Root (props) {
   return(
       <ol>
-        <li><span className="list-header"><h3>Root</h3></span>
+        <li>
+          <div className="list-header">
+            <h3>Root</h3>
+            <OptionsControl />
+          </div>
           <MapPayload payload={props.payload}/>
         </li>
       </ol>
